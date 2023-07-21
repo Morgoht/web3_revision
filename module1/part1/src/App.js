2ed4b664d60154a26aceba92a427b366f6c6ab68
Morgoht <amauryandrade1998@gmail.com>
2023-07-21 17:45:01 +0200

    courseinfo step 2

---
+++ module1/part1/src/App.js
@@ -0,0 +1,27 @@
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+const Footer = () => {
+  return(
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai </a>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name ='Amaury' age = {24} />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
