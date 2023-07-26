573b8f85f1520edf2c4bd4447f0f9468b3597dde
Amaury Andrade <amaury.andrade@student.vinci.be>
2023-07-26 11:43:07 +0200

    finish part1

--- module1/courseinfo/src/components/Header/Header.jsx
+++ module1/courseinfo/src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
+import logo from '../../img/logoVinci.png'
+
+
 const Header = (props) => {
     return (
+    <>    
         <h1>{props.course.name}</h1>
+        <img src = {logo} alt = "Logo Vinci"/>
+    </>
     )
   }
 
