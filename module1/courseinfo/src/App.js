35b5767eb1aec147370a8297e7811e5d68058511
Amaury Andrade <amaury.andrade@student.vinci.be>
2023-07-26 11:07:06 +0200

    refactor-module

--- module1/courseinfo/src/App.js
+++ module1/courseinfo/src/App.js
@@ -1,54 +1,30 @@
-
+import Header from 'components/Header/Header'
+import Content from 'components/Content/Content'
+import Total from 'components/Total/Total'
 
 const App = () => {
-  const course = 'Half stack application development'
-  const part1 = 'fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const exoCours = [10, 7, 14]
-  
-  const Header = (props) => {
-    return (
-      <>
-        <h1>{props.course}</h1>
-      </>
-    )
-  }
-  
-  const Content = (props) => {
-    return (
-      <>
-        <Part part = {part1} exercises = {exercises1} />
-        <Part part = {part2} exercises = {exercises2} />
-        <Part part = {part3} exercises = {exercises3} />
-        
-      </>
-    )
-  }
-  const Part = (props) => {
-    return (
-      <>
-        <p>{props.part} {props.exercises}</p>
-      </>
-    )
-  }
-  
-  const Total = (props) => {
-    return (
-      <>
-        <p> Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}</p>
-      </>
-    )
-  }
-  
+  const course = {
+    name: 'Half Stack application development',
+    parts:  [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+    {
+     name: 'Using props to pass data',
+     exercises: 7
+   },
+   {
+     name: 'State of component',
+     exercises: 14
+    }
+  ]
+}
   return (
   <div>
     <Header course = {course}/>
-    <Content part = {part1} exercises = {exercises1} />
-    <Total exercises = {exoCours}  />
+    <Content part = {course.parts} />
+    <Total parts = {course.parts} />
     
   </div>  
   )
---
+++ module1/courseinfo/src/components/Content/Content.jsx
@@ -0,0 +1,16 @@
+import Part from 'components/Part/Part'
+
+const Content = (props) => {
+  console.log(props)
+    return (
+      <>
+        <Part part = {props.part[0].name} exercises = {props.part[0].exercises} />
+        <Part part = {props.part[1].name} exercises = {props.part[1].exercises} />
+        <Part part = {props.part[2].name} exercises = {props.part[2].exercises} />
+        
+        
+      </>
+    )
+  }
+
+  export default Content
